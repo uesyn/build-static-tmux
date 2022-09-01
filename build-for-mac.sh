1b59d4f683f78d58382570b86d0a95a2e90aea62
uesyn <suemura@zlab.co.jp>
2022-09-02 00:27:09 +0900

    fixup! fix

--- build-for-mac.sh
+++ build-for-mac.sh
@@ -35,8 +35,7 @@ rm -rf tmux
 mkdir -p tmux
 cd tmux
 curl -sSLf -o - https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz | tar xzf - --strip-component=1
-#./configure --prefix=${BREW_PREFIX} --disable-utf8proc \
-./configure --prefix=${SCRIPT_ROOT} --disable-utf8proc \
+./configure --prefix=${BREW_PREFIX} --disable-utf8proc \
   CFLAGS="-I${SCRIPT_ROOT}/opt/include -arch arm64 -arch x86_64" \
   LDFLAGS="-L${SCRIPT_ROOT}/opt/lib" \
   CPPFLAGS="-I${SCRIPT_ROOT}/opt/include" \
@@ -45,6 +44,6 @@ curl -sSLf -o - https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/t
   LIBNCURSES_CFLAGS="-I${SCRIPT_ROOT}/opt/include" \
   LIBNCURSES_LIBS="-L${SCRIPT_ROOT}/opt/lib -lncurses" \
   LIBTINFO_CFLAGS="-I${SCRIPT_ROOT}/opt/include/ncurses" \
-  LIBTINFO_LIBS="-L${SCRIPT_ROOT}/opt/lib -ltinfo" --enable-static
+  LIBTINFO_LIBS="-L${SCRIPT_ROOT}/opt/lib -ltinfo"
 make
 cd ..
